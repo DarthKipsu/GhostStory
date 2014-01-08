4a5a206cdcd8630a2e8c3a1463f5c72fda44da96
Verna Koskinen <darth.kipsu@gmail.com>
2014-01-08 21:04:22 +0200

    add first panel

--- script/script.js
+++ script/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	$('.story').fadeIn(2000);
+	$('#panel1').fadeIn(2000);
 });
+
+function close1() {
+	$('#panel1').hide();
+	$('#panel2').fadeIn(500);
+	console.log(getName().userName);
+};
+
+// Get name and age info from user
+function getName() {
+	var name = document.getElementById('name-input').value;
+		age = document.getElementById('age-input').value;
+		name1 = name.substring(0,1);
+		name2 = name.substring(1,name.length);
+		name = (name1.toUpperCase() + name2.toLowerCase());
+	return {userName: name, userAge: age}
+};
